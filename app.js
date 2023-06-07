2f1be4316047ab17df1e545d9f17002c9765cc91
prajvalgh <prajvalgh12@gmail.com>
2023-06-07 15:13:12 +0000

    changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http')
 const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-	res.writeHad(200, { 'content-type' : text/html' })
+	res.writeHad(200, { 'content-type' : 'text/html' })
 	fs.createReadStream('index.html').pipe(res)
 })
 
