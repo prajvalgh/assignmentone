fbb5eadbda5f0e88cfd234c27b48592d844c1d67
prajvalgh <prajvalgh12@gmail.com>
2023-06-07 15:12:00 +0000

    change

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const http = require('http')
-const fs = require('fs)
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
 	res.writeHad(200, { 'content-type' : text/html' })
