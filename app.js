7fa868b3729d2a13319427815e3e72205f88e21d
prajvalgh <prajvalgh12@gmail.com>
2023-06-07 16:27:23 +0000

    changes

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-const http = require('http')
-const fs = require('fs')
+var http = require('http');
+var fs = require('fs');
 
-const server = http.createServer((req, res) => {
-	res.writeHead(200, { 'content-type' : 'text/html' })
-	fs.createReadStream('index.html').pipe(res)
-})
+const PORT=8082; 
 
-server.listen(process.env.PORT || 3000)
+fs.readFile('./index.html', function (err, html) {
+
+    if (err) throw err;    
+
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(PORT);
+});
